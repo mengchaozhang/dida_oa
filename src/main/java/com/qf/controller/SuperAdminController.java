e39995e22568efd057fdd165c01bd0d92145173c
YUBO <850274740@qq.com>
2019-11-12 14:45:32 +0800

    yubo

--- src/main/java/com/qf/controller/SuperAdminController.java
+++ src/main/java/com/qf/controller/SuperAdminController.java
@@ -1,7 +1,29 @@
 package com.qf.controller;
 
+import com.qf.service.SuperAdmin_ClassesService;
+import com.qf.service.SuperAdmin_ClassesService_impl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SuperAdminController {
+    @Autowired
+    private SuperAdmin_ClassesService superAdmin_classesService;
+
+    public SuperAdmin_ClassesService getSuperAdmin_classesService() {
+        return superAdmin_classesService;
+    }
+
+    public void setSuperAdmin_classesService(SuperAdmin_ClassesService superAdmin_classesService) {
+        this.superAdmin_classesService = superAdmin_classesService;
+    }
+
+
+
+    //功能实现
+    @RequestMapping("KCGL")
+    public String KCHHL(){
+        return "";
+    }
 }
